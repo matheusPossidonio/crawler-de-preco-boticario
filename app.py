bf4eb73522c0768025414e93f6d02b2bad8bf9c3
matheusP <matheuspgvc.mp@gmail.com>
2025-05-23 18:06:16 -0300

    alterando app.py

--- app.py
+++ app.py
@@ -71,4 +71,6 @@ def consultar_produto():
         return jsonify({"erro": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
